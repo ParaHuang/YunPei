24decf41060f7f35e1cde65584c9b6fb9b872224
ParaHuang <paradox041@gmail.com>
2026-04-05 21:00:09 +0800

    update poly3

--- pythonProject1/poly3.py
+++ pythonProject1/poly3.py
@@ -1,12 +1,12 @@
 
 
-for x in "hello":
-    print(x)
-
-print()
-
-for x in [11,22,33]:
-    print(x)
+# for x in "hello":
+#     print(x)
+#
+# print()
+#
+# for x in [11,22,33]:
+#     print(x)
 
 # for...in 的要求
 # 1.__iter__            return itself as an iterator
@@ -26,10 +26,27 @@ class Reverse:
         self.index -= 1
         return self.data[self.index]
 
-r1 = Reverse("hello~")
-# r2 = ReverseSkip1("hello~")
+class ReverseSkip1:
+    def __init__(self,data):
+        self.data = data        # data
+        self.index = len(data)  # 5
 
-for x in r1:
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index <= 1:
+            raise StopIteration
+        if self.index == len(self.data):
+            self.index -= 1
+        else:
+            self.index -= 2
+        return self.data[self.index]
+
+r1 = Reverse("hellosdkljfskd~")
+r2 = ReverseSkip1("hellosdkljfskd~")
+
+for x in r2:
     print(x)
 
 # ReversSkip1
